119732c3ff75f167b63c6ca097cdbf21d8f2a1ac
Ali <Alialaliwat21@gmail.com>
2026-06-24 01:38:23 +0300

    finished it

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
 const choicesArr =[]
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+let winner = false
 /*-------------------------------- Variables --------------------------------*/
 let currentPlayer="X"
 /*------------------------ Cached Element References ------------------------*/
@@ -10,8 +22,16 @@ let resety = document.querySelector('#reset-button')
 /*----------------------------- Event Listeners -----------------------------*/
 sqrs.forEach(function(box) {
     box.addEventListener('click', function(){
-        if(box.textContent === ''){
+        if(box.textContent === '' && winner === false) {
         box.textContent = currentPlayer
+
+    checkWinner()
+
+if (winner === true) {
+  message.textContent = `${currentPlayer} wins! Loser is obliterated`
+  return
+}
+
         if(currentPlayer === 'X'){
             currentPlayer = 'O'
         }
@@ -19,13 +39,13 @@ sqrs.forEach(function(box) {
             currentPlayer = 'X'
         }
     }
-        message.textContent = `It's ${currentPlayer}'s Turn`
+        message.textContent = `It's ${currentPlayer}'s Turn!!!`
     resety.addEventListener('click', function() {
     currentPlayer = 'X'
     sqrs.forEach(function(box) {
         box.textContent = ''
-    
-    message.textContent = `It's ${currentPlayer}'s Turn`
+    winner = false
+    message.textContent = `It's ${currentPlayer}'s Turn!!!`
 // message.textContent = `It's ${currentPlayer}'s turn`
 // }
 // resety.addEventListener('click', function(){
@@ -39,3 +59,14 @@ sqrs.forEach(function(box) {
 
     })})
 /*-------------------------------- Functions --------------------------------*/
+function checkWinner() {
+  winningCombos.forEach(function(combos) {
+    const a = sqrs[combos[0]].textContent
+    const b = sqrs[combos[1]].textContent
+    const c = sqrs[combos[2]].textContent
+
+    if (a !== '' && a === b && b === c) {
+      winner = true
+    }
+  })
+}
